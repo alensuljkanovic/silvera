540e6f49424d2001195a254f9d0dd0c1bb484b71
Alen <alienized91@gmail.com>
2017-02-05 21:48:14 +0100

    Add templates for Java server side

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -8,9 +8,18 @@ from talkie.lang.meta import get_metamodel
 from talkie.utils import get_root_path
 
 
-def test_simple():
+def test_simple1():
     metamodel = get_metamodel()
     path = os.path.join(get_root_path(), "tests", "examples", "simple_interface.tl")
+    model = metamodel.model_from_file(path)
+
+    generator = TalkieGenerator(model)
+    generator.generate()
+
+
+def test_simple2():
+    metamodel = get_metamodel()
+    path = os.path.join(get_root_path(), "tests", "examples", "simple_interface2.tl")
     model = metamodel.model_from_file(path)
 
     generator = TalkieGenerator(model)
